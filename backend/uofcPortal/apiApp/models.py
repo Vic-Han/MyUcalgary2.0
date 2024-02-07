2dc794d58430d833c881eb3ca5649c350d41b91b
Mohammed Allam <mohammed.allam@nutrien.com>
2024-02-06 20:11:47 -0700

    update

--- backend/uofcPortal/apiApp/models.py
+++ backend/uofcPortal/apiApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email
+

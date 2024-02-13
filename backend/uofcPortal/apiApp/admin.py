2de9671d15a8952a0ec59af23afb38dd4041576a
Max Trainor <98374514+MaxTrainorUofC@users.noreply.github.com>
2024-02-13 09:01:30 -0700

    Added Tables
    added Course
    added Instructor
    added Lecture
    added Grade
    added some notes

--- backend/uofcPortal/apiApp/admin.py
+++ backend/uofcPortal/apiApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Faculty, Department, Program, Student, Course, Instructor, Lecture, Grade
 
 # Register your models here.
+admin.site.register(Faculty)
+admin.site.register(Department)
+admin.site.register(Program)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Instructor)
+admin.site.register(Lecture)
+admin.site.register(Grade)

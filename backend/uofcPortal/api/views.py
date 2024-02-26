924eceb8c97c0dff5b537851e13fb87732ac204d
Mohammed Allam <mohammed.allam@nutrien.com>
2024-02-26 09:00:01 -0700

    CROS, and modifications

--- backend/uofcPortal/api/views.py
+++ backend/uofcPortal/api/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 from .models import Student, Faculty, Department, Program
 from .serializers import StudentSerializer, UserSerializer, FacultySerializer, DepartmentSerializer, ProgramSerializer
 
-
 # Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -19,20 +18,24 @@ class StudentViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    def get_queryset(self):
+        # Assuming the user is linked to the student via a ForeignKey
+        return Student.objects.filter(user=self.request.user)
+
 class FacultyViewSet(viewsets.ModelViewSet):
     queryset = Faculty.objects.all()
     serializer_class = FacultySerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    # authentication_classes = (TokenAuthentication,)
+    # permission_classes = (IsAuthenticated,)
 
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    # authentication_classes = (TokenAuthentication,)
+    # permission_classes = (IsAuthenticated,)
 
 class ProgramViewSet(viewsets.ModelViewSet):
     queryset = Program.objects.all()
     serializer_class = ProgramSerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    # authentication_classes = (TokenAuthentication,)
+    # permission_classes = (IsAuthenticated,)

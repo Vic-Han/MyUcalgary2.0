f507603e02e00f97b9081c6c2c765bded89a172e
Victor Han <victor.han72@gmail.com>
2024-02-27 08:43:47 -0700

    Minor fixes

--- frontend/src/components/AppNavbar.vue
+++ frontend/src/components/AppNavbar.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="flex flex-col w-96 bg-white rounded-lg shadow-md fixed inset-0 h-screen p-8"> <!-- Added shadow effect -->
-    <img src="@/assets/unilogo.png" alt="University Logo" class="h-16 my-4 self-center">
+    <img src="@/assets/unilogo.png" alt="University Logo" class="h-24 my-4 self-center">
     
     <div class="flex flex-col mt-4 space-y-4"> <!-- Increased vertical spacing -->
       <!-- Updated class name to nav-link -->
       <router-link to="/dashboard" class="group flex flex-row items-center p-4 
-      border border-white-300 shadow-sm mt-4 rounded-lg text-black-100" v-bind:class="{'bg-red-100 text-white-100' : selected === 'dashboard'}">
+      border border-white-300 shadow-md mt-4 rounded-xl text-black-100" v-bind:class="{'bg-red-100 text-white-100' : selected === 'dashboard'}">
         <img src="@/assets/dashboard.jpg" alt="Dashboard" class="h-10 mx-8">
         <span class="text-2xl">Dashboard</span>
       </router-link>
       
       <router-link to="/academics" class="group flex flex-row items-center p-4 
-      border border-white-300 shadow-sm mt-4 rounded-lg" v-bind:class="{'bg-red-100 text-white-100' : selected === 'academics'}">
+      border border-white-300 shadow-md mt-4 rounded-xl" v-bind:class="{'bg-red-100 text-white-100' : selected === 'academics'}">
         <img src="@/assets/Academics.webp" alt="Academics" class="h-10 mx-8">
         <span class="text-2xl group-hover:text-white focus:text-white">Academics</span>
       </router-link>
       
-      <router-link to="/application" class="group flex flex-row items-center p-4 border border-white-300 shadow-sm mt-4 rounded-lg"
+      <router-link to="/application" class="group flex flex-row items-center p-4 border border-white-300 shadow-md mt-4 rounded-xl"
       v-bind:class="{'bg-red-100 text-white-100' : selected === 'application'}">
         <img src="@/assets/application.png" alt="Application" class="h-10 mx-8">
         <span class="text-2xl group-hover:text-white focus:text-white">Applications</span>
       </router-link>
       
-      <router-link to="/misc" class="group flex flex-row items-center p-4 border border-white-300 shadow-sm mt-4 rounded-lg"
+      <router-link to="/misc" class="group flex flex-row items-center p-4 border border-white-300 shadow-md mt-4 rounded-xl"
       v-bind:class="{'bg-red-100 text-white-100' : selected === 'misc'}">
         <img src="@/assets/aroundU.png" alt="Around U" class="h-10 mx-8">
         <span class="text-2xl group-hover:text-white focus:text-white">Around U</span>

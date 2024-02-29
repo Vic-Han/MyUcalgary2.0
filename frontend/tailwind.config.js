c708093e54497b7ad130b03a483d4ac6c8ab85ea
CallumM51 <callum.matheson1@ucalgary.ca>
2024-02-28 17:09:45 -0700

    Added Standard Colours
    If you use colours other than those found in tailwind.config Vic will slaughter your entire bloodline.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,11 +7,10 @@ module.exports = {
       colors: {
         'white': {
           100: '#FFFFFF',
-          200: '#E5E5E5',
-          300: '#D9D9D9',
+          200: '#F0F0F0',
         },
         'grey' :{
-          100: '#F3F4F6',
+          100: '#D9D9D9',
           200: '#6D6E71',
         },
         'black' : {
@@ -20,18 +19,46 @@ module.exports = {
         'red' : {
           100: '#D6001C',
         },
-        'primary': '#FFFFFF',
-        'secondary': {
-          100: '#E2E2D5',
-          200: '#888883',
-        },  
-      },
-      dropShadow: {
-        'sm' : '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
-        'md' : '0 4px 6px -1px rgba(0, 0, 0, 0.1)',
-        'lg' : '0 10px 15px -3px rgba(0, 0, 0, 0.1)',
-        'xl' : '0 20px 25px -5px rgba(0, 0, 0, 0.1)',
-        '2xl' : '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+        'yellow' :{
+          100: '#FFCD00',
+        },
+        'rubine': {
+          100: '#ED0A72',
+        },
+        'orange':{
+          100: '#FFA300',
+        },
+        'open': '#92BDD6',
+        'closed': '#FF0000',
+        'waitlist': '#FFCD00',
+        'grade' : {
+          'A' : '#47A67C',
+          'A-background'  : '#08482C',
+          'A-text': '#FFFFFF',
+          'B': '#FFCD00',
+          'B-background' : '#9D8830',
+          'B-text': '#6D6E71',
+          'C': '#FFA300',
+          'C-background' : '#976B1C',
+          'C-text': '#6D6E71',
+          'D': '#D6001C',
+          'D-background' : '#721420',
+          'D-text': '#FFFFFF',
+          'F': '#D6001C',
+          'F-background' : '#721420',
+          'F-text': '#FFFFFF',
+        },
+        'course': {
+          100: '#DEEC89',
+          200: '#EED785',
+          300: '#A9DD91',
+          400: '#FC8787',
+          500: '#A2D4F0',
+          600: '#F4BEEF',
+          700: '#FDBED4',
+          800: '#C9C1FC',
+          900: '#98C3F6',
+        },
       }
     },
   },

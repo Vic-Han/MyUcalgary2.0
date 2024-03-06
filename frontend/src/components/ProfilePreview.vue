206406c1597b9eb7f4be9e8297e5844246c4afb3
kusumpreetheer <55485344+kusumpreetheer@users.noreply.github.com>
2024-03-05 19:32:23 -0700

    fixed profile and search

--- frontend/src/components/ProfilePreview.vue
+++ frontend/src/components/ProfilePreview.vue
@@ -1,11 +1,10 @@
 <template>
-    <div class = "sub-component">
-        This is the ProfilePreview Component
-        <router-link to = "/profile" class = "btn">
-            Go checkout your profile
-        </router-link>
-    </div>
-</template>
+    <router-link to="/profile" class="flex items-center no-underline text-black font-bold">
+        <div class="flex items-center justify-center w-12 h-12 rounded-full bg-gray-100 mr-2">
+            CM
+        </div>
+    </router-link>
+</template>  
 
 <script>
     export default{

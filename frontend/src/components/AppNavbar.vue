1949f12c5e99b001f9f63312a3076d0264e76728
Callum Matheson <81483800+CallumM51@users.noreply.github.com>
2024-03-16 19:40:49 -0600

    Added Info popup
    - Added info popup to contact IT/Team's rights
    
    - Commented out personal settings from route

--- frontend/src/components/AppNavbar.vue
+++ frontend/src/components/AppNavbar.vue
@@ -38,15 +38,31 @@
     </div>
     
     <div class="mt-auto flex justify-between w-full p-10"> 
-      <div>
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 fill-grey-200" viewBox="0 -960 960 960">
+      <div class="flex flex-col">
+        <div v-if="viewInfo" class="absolute -translate-x-1/3 -translate-y-36 h-32 w-40 bg-white-100 border border-white-300  shadow-lg rounded-lg">
+          <div class="font-semibold">Contact IT</div>
+          <div class="flex flex-row">
+            <div class="text-sm mr-1 w-1/4 text-right text-grey-200">P:</div>
+           <div class="text-sm text-grey-200">(403)-210-9300</div>
+          </div>
+          <div class="flex flex-row pb-5">
+            <div class="text-sm mr-1 w-1/4 text-right text-grey-200">E:</div>
+           <a href="mailto:it@ucalgary.ca" class="text-sm hover:underline text-grey-200">it@ucalgary.ca</a>
+          </div>
+          <a href="https://github.com/Vic-Han/SENG-401-Project/graphs/contributors" class="text-xs hover:underline text-grey-200">&copy; OhMyUCalgary</a>
+          <div class="text-xs text-grey-200"> All rights Reserved.</div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" @click="viewInfo = !viewInfo" class="h-10 w-10 fill-grey-200 hover:fill-red-100" viewBox="0 -960 960 960">
           <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
+          <title>Info</title>
         </svg>
       </div>
-      
-      <svg xmlns="http://www.w3.org/2000/svg" @click="logout" class="h-10 w-10 fill-grey-200" viewBox="0 -960 960 960">
-        <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/>
-      </svg>
+      <div class="has-tooltip">
+        <svg xmlns="http://www.w3.org/2000/svg" @click="logout" class="h-10 w-10 fill-grey-200 hover:fill-red-100" viewBox="0 -960 960 960">
+          <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/>
+          <title>Logout</title>
+        </svg>
+      </div>
     </div>
   </div>
 </template>
@@ -60,6 +76,11 @@ export default {
       return this.selected === link
     }
   },
+  data() {
+      return {
+        viewInfo: false
+      }
+    },
   methods: {
     logout(){
       this.$cookies.remove('auth-token')

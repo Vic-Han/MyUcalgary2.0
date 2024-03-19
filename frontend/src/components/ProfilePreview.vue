97812d322f6e2f921166949c0becea2f944af6d5
Victor Han <victor.han72@gmail.com>
2024-03-19 15:03:46 -0600

    Used cookie for initials

--- frontend/src/components/ProfilePreview.vue
+++ frontend/src/components/ProfilePreview.vue
@@ -1,13 +1,24 @@
 <template>
     <router-link to="/profile" class="flex items-center no-underline text-black font-bold">
         <div class="flex items-center justify-center w-12 h-12 rounded-full bg-gray-100 mr-2 text-rubine-100 border border-rubine-100">
-            CM
+            {{initials}}
         </div>
     </router-link>
 </template>  
 
 <script>
+
+
     export default{
-        name : 'ProfilePreview'
+        name : 'ProfilePreview',
+        data: ()=>{
+            return {
+                initials: ""
+            }
+        },
+        created(){
+            this.initials = this.$cookies.get('initials').toUpperCase()
+        }
     }
+   
 </script>

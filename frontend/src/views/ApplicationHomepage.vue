be4b811852f78c04a68446db50e9d056d27ed0a3
Callum Matheson <81483800+CallumM51@users.noreply.github.com>
2024-03-24 11:29:32 -0600

    Updated applications

--- frontend/src/views/ApplicationHomepage.vue
+++ frontend/src/views/ApplicationHomepage.vue
@@ -1,25 +1,25 @@
 <template> 
   <div>
       <ApplicationForm v-if="applicationForm" @cancel="closeApplicationForm" @submit="closeApplicationForm"/>
-      <div v-else class="bg-white-100 w-full rounded-xl">
+      <div v-else class="bg-white-100 w-full rounded-xl shadow-xl">
         <div class="text-left p-5 text-3xl text-grey-200"> My Applications </div>
         <div class = "flex flex-row px-20">
-          <div class="border border-grey-200 rounded-tl-lg w-1/4 py-3 bg-gray-300" 
+          <div class="border border-grey-200 rounded-tl-lg w-1/4 py-3 cursor-pointer bg-gray-300" 
           @click="selected='ugrad'" v-bind:class="{'bg-white-100 border-b-0' : selected==='ugrad'}"> Undergrad </div>
-          <div class="border border-grey-200 w-1/4 py-3 bg-gray-300" 
+          <div class="border border-grey-200 w-1/4 py-3 cursor-pointer bg-gray-300" 
           @click="selected='award'" v-bind:class="{'bg-white-100 border-b-0' : selected==='award'}"> Awards </div>
-          <div class="border border-grey-200 w-1/4 py-3 bg-gray-300" 
+          <div class="border border-grey-200 w-1/4 py-3 cursor-pointer bg-gray-300" 
           @click="selected='scholarship'" v-bind:class="{'bg-white-100 border-b-0' : selected==='scholarship'}"> Scholarships </div>
-          <div class="border border-grey-200 rounded-tr-lg w-1/4 py-3 bg-gray-300" 
+          <div class="border border-grey-200 rounded-tr-lg w-1/4 py-3 cursor-pointer bg-gray-300" 
           @click="selected='grad'" v-bind:class="{'bg-white-100 border-b-0' : selected==='grad'}"> Grad</div>
         </div>
         <div class = "px-20"> 
           <div class="p-5 h-144 box-content border-x border-grey-200 border-b rounded-b-xl flex-wrap" v-if="selected === 'ugrad'">
-            <div class="flex flex-row overflow-x-auto bg-white-200 p-1 h-full rounded-xl">
-            <div v-if="ugradApps.length == 0" class="text-7xl text-grey-200 p-16 leading-loose">
+            <div class="flex flex-row overflow-x-auto bg-white-200 shadow-inner p-1 h-full rounded-xl">
+            <div v-if="ugradApps.length == 0" class="text-4xl text-center w-full text-grey-200 p-16 leading-loose">
               No applications for undergrad programs. You can start an application below.
             </div>
-            <div v-for="(app,index) in ugradApps" :key="index" class="bg-grey-100 rounded-lg p-5 h-fit w-fit ml-12 mb-2 mt-12">
+            <div v-for="(app,index) in ugradApps" :key="index" class="bg-grey-100 rounded-lg shadow-xl p-5 h-fit w-fit ml-12 mb-2 mt-12">
               <div class="flex flex-row justify-between w-144">
                 <div class="text-3xl"> {{ app.program }}</div>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" class="h-14 w-14 fill-grey-200 hover:fill-red-100" @click="deleteApplication(app.ID)">
@@ -27,7 +27,7 @@
                 </svg>
               </div>
               <div class="my-1 text-xl" :class="statusColor(app.status)">Status: {{app.status}}</div>
-              <img src="../assets/UndergradApplication.png" alt="graduation cap" class="h-80 w-144 square rounded-xl shadow-md mt-5"/>
+              <img src="../assets/UndergradApplication.png" alt="graduation cap" class="h-80 w-144 square rounded-xl mt-5"/>
               
               <div class='relative'>
                 <div class="absolute opacity-80 -top-80 left-144 -translate-x-full bg-white-100 text-black-100 text-xl w-96 py-5 px-10 text-left rounded-xl">
@@ -42,11 +42,11 @@
             </div>
           </div>
           <div class="p-5 h-144 box-content border-x border-grey-200 border-b rounded-b-xl flex-wrap" v-else-if="selected === 'award'">
-            <div class="flex flex-row overflow-x-auto bg-white-200 p-1 h-full rounded-xl">
-            <div v-if="awards.length == 0" class="text-7xl text-grey-200 p-20 leading-loose">
+            <div class="flex flex-row overflow-x-auto bg-white-200 shadow-inner p-1 h-full rounded-xl">
+            <div v-if="awards.length == 0" class="text-4xl text-center w-full text-grey-200 p-20 leading-loose">
               No applications for awards. You can start an application below.
             </div>
-            <div v-for="(award,index) in awards" :key="index" class="bg-grey-100 rounded-lg p-5 h-fit w-fit ml-12 mb-2 mt-12">
+            <div v-for="(award,index) in awards" :key="index" class="bg-grey-100 shadow-xl rounded-lg p-5 h-fit w-fit ml-12 mb-2 mt-12">
               <div class="flex flex-row p-2 justify-between w-144">
                 <div class="text-3xl"> {{ award.name }}</div>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" class="h-14 w-14 fill-grey-200 hover:fill-red-100" @click="deleteApplication(award.ID)">
@@ -54,7 +54,7 @@
                 </svg>
               </div>
               <div class="my-1 text-xl" :class="statusColor(award.status)">Status: {{award.status}}</div>
-              <img src="../assets/AwardApplication.png" alt="graduation cap" class="h-80 w-144 square rounded-xl shadow-md mt-5"/>
+              <img src="../assets/AwardApplication.png" alt="graduation cap" class="h-80 w-144 square rounded-xl mt-5"/>
               
               <div class='relative'>
                 <div class="absolute opacity-80 top-0 -translate-y-full left-144 -translate-x-full bg-white-100 text-black-100 text-xl w-96 py-5 px-10 text-left rounded-xl">
@@ -66,11 +66,11 @@
             </div>
           </div>
           <div class=" p-5 h-144 box-content border-x border-grey-200 border-b rounded-b-xl" v-else-if="selected === 'scholarship'">
-            <div class="overflow-x-auto bg-white-200 p-1 flex flex-row h-full rounded-xl">
-              <div v-if="scholarships.length === 0" class="text-7xl text-grey-200 p-20 leading-loose">
+            <div class="overflow-x-auto bg-white-200 shadow-inner p-1 flex flex-row h-full rounded-xl">
+              <div v-if="scholarships.length === 0" class="text-4xl text-center w-full text-grey-200 p-20 leading-loose">
                 No applications for scholarships. You can start an application below.
               </div>
-            <div v-for="(scholarship,index) in scholarships" :key="index" class="bg-grey-100 rounded-lg p-5 h-fit w-fit ml-12 mb-2 mt-12">
+            <div v-for="(scholarship,index) in scholarships" :key="index" class="bg-grey-100 shadow-xl rounded-lg p-5 h-fit w-fit ml-12 mb-2 mt-12">
               <div class="flex flex-row p-2 justify-between w-144">
                 <div class="text-3xl"> {{ scholarship.name }}</div>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" class="h-14 w-14 fill-grey-200 hover:fill-red-100" @click="deleteApplication(scholarship.ID)">
@@ -78,7 +78,7 @@
                 </svg>
               </div>
               <div class="my-1 text-xl" :class="statusColor(scholarship.status)">Status: {{scholarship.status}}</div>
-              <img src="../assets/ScholarshipApplication.png" alt="graduation cap" class="h-80 w-144 square rounded-xl shadow-md mt-5"/>
+              <img src="../assets/ScholarshipApplication.png" alt="graduation cap" class="h-80 w-144 square rounded-xl mt-5"/>
               
               <div class='relative'>
                 <div class="absolute opacity-80 -top-80 left-144 -translate-x-full bg-white-100 text-black-100 text-xl w-96 py-5 px-10 text-left rounded-xl">
@@ -90,11 +90,11 @@
             </div>
           </div>
           <div class="px-5 py-10 h-144 box-content border-x border-grey-200 border-b rounded-b-xl " v-else-if="selected === 'grad'">
-            <div class="overflow-x-auto bg-white-200 p-1 flex flex-row h-full rounded-xl">
-              <div v-if="gradApps.length == 0" class="text-7xl text-grey-200 p-18 leading-loose">
+            <div class="overflow-x-auto bg-white-200 shadow-inner p-1 flex flex-row h-full rounded-xl">
+              <div v-if="gradApps.length == 0" class="text-4xl text-center w-full text-grey-200 p-18 leading-loose">
                 No applications for graduate programs. You can start an application below.
               </div>
-              <div v-for="(app,index) in gradApps" :key="index" class="bg-grey-100 rounded-lg p-5 h-fit w-fit ml-12 mb-2 mt-12">
+              <div v-for="(app,index) in gradApps" :key="index" class="bg-grey-100 rounded-lg shadow-xl p-5 h-fit w-fit ml-12 mb-2 mt-12">
               <div class="flex flex-row p-2 justify-between w-144">
                 <div class="text-3xl"> {{ app.program }}</div>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" class="h-14 w-14 fill-grey-200 hover:fill-red-100" @click="deleteApplication(app.ID)">
@@ -102,7 +102,7 @@
                 </svg>
               </div>
               <div class="my-1 text-xl" :class="statusColor(app.status)">Status: {{app.status}}</div>
-              <img src="../assets/GraduateApplication.png" alt="graduation cap" class="h-80 w-144 square rounded-xl shadow-md mt-5"/>
+              <img src="../assets/GraduateApplication.png" alt="graduation cap" class="h-80 w-144 square rounded-xl mt-5"/>
               
               <div class='relative'>
                 <div class="absolute opacity-80 -translate-y-full -top-1 left-1 bg-white-100 text-black-100 text-xl w-96 py-5 px-10 text-left rounded-xl">

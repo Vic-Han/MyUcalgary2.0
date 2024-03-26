d9f39433237e259db249ecf6ea4bd744f4b736fb
Callum Matheson <81483800+CallumM51@users.noreply.github.com>
2024-03-25 22:09:07 -0600

    Update FinanceBreakdown.vue

--- frontend/src/views/FinanceBreakdown.vue
+++ frontend/src/views/FinanceBreakdown.vue
@@ -33,11 +33,11 @@
                 <svg class="absolute right-3 top-1/2 transform -translate-y-1/2 w-5 h-5 pointer-events-none text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
             </div>
                 <div class="grid grid-cols-5">
-                  <div class=" text-center rounded-tl-xl px-4 py-4 bg-grey-100 border border-gray-300 cursor-pointer" v-bind:class="{'bg-white-100 border-b-0' : activeTab =='all'}" @click="activeTab = 'all' ">All</div>
-                  <div class=" text-center px-4 py-4 bg-grey-100 border border-gray-300 cursor-pointer" v-bind:class="{'bg-white-100 border-b-0' : activeTab =='fee'}" @click="activeTab = 'fee' ">Fees</div>
-                  <div class=" text-center px-4 py-4 bg-grey-100 border border-gray-300 cursor-pointer" v-bind:class="{'bg-white-100 border-b-0' : activeTab =='payment'}" @click="activeTab = 'payment'">Payments</div>
-                  <div class=" text-center px-4 py-4 bg-grey-100 border border-gray-300 cursor-pointer" v-bind:class="{'bg-white-100 border-b-0' : activeTab =='scholarship'}" @click=" activeTab = 'scholarship'">Scholarships</div>
-                  <div class=" text-center rounded-tr-xl px-4 pt-4 pb-4 bg-grey-100 border border-gray-300 cursor-pointer" v-bind:class="{'bg-white-100 border-b-0' : activeTab =='award'}" @click="activeTab = 'award'">Awards</div>
+                  <div class=" text-center rounded-tl-xl px-4 py-4 bg-gray-300 border border-gray-300 cursor-pointer" v-bind:class="{'bg-white-100 border-b-0' : activeTab =='all'}" @click="activeTab = 'all' ">All</div>
+                  <div class=" text-center px-4 py-4 bg-gray-300 border border-gray-300 cursor-pointer" v-bind:class="{'bg-white-100 border-b-0' : activeTab =='fee'}" @click="activeTab = 'fee' ">Fees</div>
+                  <div class=" text-center px-4 py-4 bg-gray-300 border border-gray-300 cursor-pointer" v-bind:class="{'bg-white-100 border-b-0' : activeTab =='payment'}" @click="activeTab = 'payment'">Payments</div>
+                  <div class=" text-center px-4 py-4 bg-gray-300 border border-gray-300 cursor-pointer" v-bind:class="{'bg-white-100 border-b-0' : activeTab =='scholarship'}" @click=" activeTab = 'scholarship'">Scholarships</div>
+                  <div class=" text-center rounded-tr-xl px-4 pt-4 pb-4 bg-gray-300 border border-gray-300 cursor-pointer" v-bind:class="{'bg-white-100 border-b-0' : activeTab =='award'}" @click="activeTab = 'award'">Awards</div>
                 </div>
                 <div class="flex flex-col rounded-lg shadow-md border-b border-r border-l border-gray-300 rounded-tl-none">
                   <div class="flex flex-row">

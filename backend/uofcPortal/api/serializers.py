48f1de3eb4b911c54778d204890b693ad114b8c7
Mohammed Allam <mohammed.allam@nutrien.com>
2024-03-26 15:40:25 -0600

    update

--- backend/uofcPortal/api/serializers.py
+++ backend/uofcPortal/api/serializers.py
@@ -20,12 +20,6 @@ class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
         fields = '__all__'
-
-class EmergencyContactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EmergencyContact
-        fields = '__all__'
-        
 class StudentSerializer(serializers.ModelSerializer):
     address = serializers.SerializerMethodField()
     emergency_contact1 = serializers.SerializerMethodField()
@@ -53,6 +47,71 @@ class StudentSerializer(serializers.ModelSerializer):
             return f"{obj.emergency_contact3.emergency_contact_name},{obj.emergency_contact3.emergency_contact_phone},{obj.emergency_contact3.emergency_contact_relationship}"
         return None
     
+
+
+class PersonalInfoSerializer(serializers.ModelSerializer):
+    personal_info = serializers.SerializerMethodField()
+    citizenship = serializers.SerializerMethodField()
+    address = serializers.SerializerMethodField()
+    phone_numbers = serializers.SerializerMethodField()
+    email = serializers.SerializerMethodField()
+    emergency_contact = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Student
+        fields = ('personal_info', 'citizenship', 'address', 'phone_numbers', 'email', 'emergency_contact')
+
+    def get_personal_info(self, obj):
+        return {
+            "firstname": obj.student_first_name,
+            "lastname": obj.student_last_name,
+            "UCID": obj.student_id,
+            "date of birth": obj.date_of_birth
+        }
+
+    def get_citizenship(self, obj):
+        return {
+            "country": obj.address.address_country,
+            "status": obj.citizenship_status
+        }
+
+    def get_address(self, obj):
+        return {
+            "street address": obj.address.address_street_address,
+            "postal code": obj.address.address_postal_code,
+            "city": obj.address.address_city,
+            "province/state": obj.address.address_province
+        }
+
+    def get_phone_numbers(self, obj):
+        return {
+            "home": obj.home_phone_number if obj.home_phone_number else None,
+            "mobile": obj.mobile_phone_number if obj.mobile_phone_number else None,
+            "other": obj.other_phone_number if obj.other_phone_number else None,
+            "preferred": obj.preferred_phone
+        }
+
+    def get_email(self, obj):
+        return {
+            "personal": obj.personal_email,
+            "school": obj.school_email, 
+            "preferred": obj.preferred_email
+        }
+
+    def get_emergency_contact(self, obj):
+        return {
+            "name1": obj.emergency_contact1.emergency_contact_name,
+            "phone1": obj.emergency_contact1.emergency_contact_phone,
+            "relation1": obj.emergency_contact1.emergency_contact_relationship,
+            "name2": obj.emergency_contact2.emergency_contact_name if obj.emergency_contact2.emergency_contact_name else None,
+            "phone2": obj.emergency_contact2.emergency_contact_phone if obj.emergency_contact2.emergency_contact_phone else None,
+            "relation2": obj.emergency_contact2.emergency_contact_relationship if obj.emergency_contact2.emergency_contact_relationship else None,
+            "name3": obj.emergency_contact3.emergency_contact_name if obj.emergency_contact3.emergency_contact_name else None,
+            "phone3": obj.emergency_contact3.emergency_contact_phone if obj.emergency_contact3.emergency_contact_phone else None,
+            "relation3": obj.emergency_contact3.emergency_contact_relationship if obj.emergency_contact3.emergency_contact_relationship else None,
+            "preferred": obj.preferred_emergency_contact
+        }
+
 class FacultySerializer(serializers.ModelSerializer):
     class Meta:
         model = Faculty
@@ -83,6 +142,11 @@ class LectureSerializer(serializers.ModelSerializer):
         model = Lecture
         fields = '__all__'
 
+class TutorialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tutorial
+        fields = '__all__'
+
 class GradeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Grade
@@ -107,4 +171,14 @@ class StudentApplicationsSerializer(serializers.ModelSerializer):
 class RequirementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Requirement
+        fields = '__all__'
+
+class TermSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Term
+        fields = '__all__'
+
+class EmergencyContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmergencyContact
         fields = '__all__'

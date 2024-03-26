9088f6c764c644b89833125881add4224621c5de
Victor Han <victor.han72@gmail.com>
2024-03-25 20:04:18 -0600

    Added template for creating real data

---
+++ backend/uofcPortal/make.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Delete the SQLite database file
+rm -f db.sqlite3
+rm -rf uofcPortal/migrations/001_*
+
+# Make migrations
+python3 manage.py makemigrations
+
+# Apply migrations
+python3 manage.py migrate
+
+# Load data
+# Replace 'your_fixture.json' with the actual name of your fixture file
+python3 manage.py loaddata dummy_data.json
+
+# Run the server
+python3 manage.py runserver

bbaa9ea7b489413193844bc2753ae9362034a08d
Max Trainor <98374514+MaxTrainorUofC@users.noreply.github.com>
2024-03-26 12:55:42 -0600

    reeenabled authentication for StudentRequirementsView
    was disabled for testing

--- backend/uofcPortal/api/views.py
+++ backend/uofcPortal/api/views.py
@@ -446,11 +446,11 @@ class DashboardView(APIView, GradeMixins):
         return Response(dashboard_data)
     
 class StudentRequirementsView(APIView):
-    #authentication_classes = (TokenAuthentication,)  # uncomment this when doing authentication
-    #permission_classes = (IsAuthenticated,)  # uncomment this when doing authentication
+    authentication_classes = (TokenAuthentication,)  # uncomment this when doing authentication
+    permission_classes = (IsAuthenticated,)  # uncomment this when doing authentication
 
     def get(self, request):
-        student = Student.objects.first()
+        student = get_object_or_404(Student, user=request.user)
         if not student:
             return Response({"error": "No student found"}, status=404)
         
